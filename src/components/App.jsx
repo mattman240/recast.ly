c65a6496ebd663f2327af9dc7ceb757cc3b75a4a
mattman240 <mattmull98@gmail.com>
2018-01-31 17:31:25 -0800

    Passing most tests getting it connected to youtube

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,50 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+// need to keep track of currentVid (this.videos[0])??
+// event function in App, listener on click which then 
+// will change the state of our current vid
+
+
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: window.exampleVideoData,
+      currVid: window.exampleVideoData[0]  
+    };
+    this.onTitleClick = this.onTitleClick.bind(this);
+  }
+  
+  onTitleClick(video) {
+    this.setState({
+      currVid: video
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><h5><em>search</em> view goes here</h5></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <div><VideoPlayer video={this.state.currVid}/></div>
+          </div>
+          <div className="col-md-5">
+            <div> <VideoList videos={this.state.videos} onTitleClick={this.onTitleClick}/> </div>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
+//test
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,24 @@
 var searchYouTube = (options, callback) => {
   // TODO
+  $.ajax({
+    url:'https://www.googleapis.com/youtube/v3/search/',
+    type: 'GET',
+    data: {
+          part: 'snippet',
+          type: 'video',
+          videoEmbeddable: 'true',
+          q: options.query,
+          maxResults: options.max,
+          key: options.key
+    },
+    success: function(data) {
+      callback(data);
+      console.log('success');
+    },
+    error: function(err) {
+      console.error(err);
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
